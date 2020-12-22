a773403d168d155f0914cb802aac55c63da5c144
b8enly <sofa512@yandex.ru>
2020-12-22 13:06:04 +0300

    201-725_ЯблонскаяСС

---
+++ Лабораторная 12/201-725_ЯблонскаяСС_Задание3_Лаб12/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace _201_725_ЯблонскаяСС_Задание3_Лаб12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if ((a > 40) || (a < 10))
+            {
+                Console.WriteLine("Invalid Data");
+            }
+            else
+            {
+                int b = a / 10;
+                int c = a % 10;
+                if (b == 1)
+                {
+                    switch (c)
+                    {
+                        case 0:
+                            Console.Write("ten ");
+                            break;
+                        case 1:
+                            Console.Write("eleven ");
+                            break;
+                        case 2:
+                            Console.Write("twelve ");
+                            break;
+                        case 3:
+                            Console.Write("thirteen ");
+                            break;
+                        case 4:
+                            Console.Write("fourteen ");
+                            break;
+                        case 5:
+                            Console.Write("fifteen ");
+                            break;
+                        case 6:
+                            Console.Write("sixteen ");
+                            break;
+                        case 7:
+                            Console.Write("seventeen ");
+                            break;
+                        case 8:
+                            Console.Write("eighteen ");
+                            break;
+                        case 9:
+                            Console.Write("nineteen ");
+                            break;
+                    }
+                }
+                else
+                {
+                    switch (b)
+                    {
+                        case 2:
+                            Console.Write("twenty ");
+                            break;
+                        case 3:
+                            Console.Write("thirty ");
+                            break;
+                        case 4:
+                            Console.Write("fourty ");
+                            break;
+                    }
+                    switch (c)
+                    {
+                        case 1:
+                            Console.Write("one ");
+                            break;
+                        case 2:
+                            Console.Write("two ");
+                            break;
+                        case 3:
+                            Console.Write("three ");
+                            break;
+                        case 4:
+                            Console.Write("four ");
+                            break;
+                        case 5:
+                            Console.Write("five ");
+                            break;
+                        case 6:
+                            Console.Write("six ");
+                            break;
+                        case 7:
+                            Console.Write("seven ");
+                            break;
+                        case 8:
+                            Console.Write("eight ");
+                            break;
+                        case 9:
+                            Console.Write("nine ");
+                            break;
+                    }
+                }
+                Console.Write("training tasks");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Лабораторная 12/201-725_ЯблонскаяСС_Задание5_Лаб12/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace _201_725_ЯблонскаяСС_Задание5_Лаб12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the year: ");
+            int x = int.Parse(Console.ReadLine());
+            if (x < 1984)
+            {
+                Console.WriteLine("Ivalid Data");
+            }
+            else
+            {
+                int color = ((x - 1984) % 60 / 12);
+                Console.WriteLine();
+                Console.Write("The year of the ");
+                switch (color)
+                {
+                    case 0:
+                        Console.Write("green ");
+                        break;
+                    case 1:
+                        Console.Write("red ");
+                        break;
+                    case 2:
+                        Console.Write("yellow ");
+                        break;
+                    case 3:
+                        Console.Write("white ");
+                        break;
+                    case 4:
+                        Console.Write("black ");
+                        break;
+                }
+                int animal = (x - 1984) % 12;
+                switch (animal)
+                {
+                    case 0:
+                        Console.Write("rat");
+                        break;
+                    case 1:
+                        Console.Write("cow");
+                        break;
+                    case 2:
+                        Console.Write("tiger");
+                        break;
+                    case 3:
+                        Console.Write("rabbit");
+                        break;
+                    case 4:
+                        Console.Write("dragon");
+                        break;
+                    case 5:
+                        Console.Write("snake");
+                        break;
+                    case 6:
+                        Console.Write("horse");
+                        break;
+                    case 7:
+                        Console.Write("sheep");
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
