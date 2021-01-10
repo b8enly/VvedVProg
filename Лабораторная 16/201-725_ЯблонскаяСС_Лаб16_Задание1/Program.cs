f557590186aec6aef17996744b7db6992200f08e
b8enly <sofa512@yandex.ru>
2021-01-10 23:43:33 +0300

    201-725_ЯблонскаяСС

--- Лабораторная 16/201-725_ЯблонскаяСС_Лаб16_Задание1/Program.cs
+++ Лабораторная 16/201-725_ЯблонскаяСС_Лаб16_Задание1/Program.cs
@@ -9,9 +9,9 @@ namespace _201_725_ЯблонскаяСС_Лаб16_Задание1
             Console.Write("Enter the power of the array: ");
             int N = int.Parse(Console.ReadLine());
             int[] massive = new int[N];
-            for (int i = 1; i < massive.Length; i++)
+            for (int i = 1; i <= massive.Length; i++)
             {
-                massive[i] = 2 * i - 1;
+                massive[i-1] = 2 * i - 1;
             }
             Console.Write("Array: ");
             for (int i = 0; i < massive.Length; i++)
